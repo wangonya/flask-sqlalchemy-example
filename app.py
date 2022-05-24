8ac87112a27b4cb7120fdd5b69db84eb43233177
wangonya <kwangonya@gmail.com>
2022-05-24 09:04:27 +0300

    add more endpoints

--- app.py
+++ app.py
@@ -5,20 +5,31 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return {"message": "Hello World!"}
 
-@app.route("/contacts/create/", methods=["POST"])
-def create_contact():
+@app.route("/users/create", methods=["POST"])
+def create_user():
     person = Person(**request.json)
     db.session.add(person)
     db.session.commit()
     return {"message": "User created"}
 
+@app.route("/contacts/create/", methods=["POST"])
+def create_contact():
+    return {"message": "Contact created"}
+
 
 @app.route("/contacts/")
 def get_all_contacts():
-    all_contacts = Person.query.all()
-    print(all_contacts)
-    return {"contacts": 1}
+    return {"message": "Contacts queried"}
+
+@app.route("/users/")
+def get_all_users():
+    users = Person.query.all()
+    print(users)
+    return {"message": "Users queried"}
 
 
 # get single contact
@@ -46,13 +57,13 @@ db = SQLAlchemy(app)
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), nullable=False)
-
-    def __repr__(self):
-        return self.first_name
+    contacts = db.relationship('Contact', backref='person', lazy=True)
 
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
+    contact = db.Column(db.String())
+    person_id = db.Column(db.Integer, db.ForeignKey('person.id'),
+        nullable=False)
 
 db.create_all()
