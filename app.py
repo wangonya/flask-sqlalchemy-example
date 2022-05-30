e26825189b1d2572539f28437e161d464e480fe9
wangonya <kwangonya@gmail.com>
2022-05-30 22:25:20 +0300

    connect fe/be

--- app.py
+++ app.py
@@ -1,35 +1,52 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return {"message": "Hello World!"}
+    return render_template("index.html")
 
-@app.route("/users/create", methods=["POST"])
-def create_user():
-    person = Person(**request.json)
+
+@app.route("/people/add", methods=["POST", "GET"])
+def create_person():
+    if request.method == "GET":
+        return render_template("create-person.html")
+
+    person = Person(**request.form)
     db.session.add(person)
     db.session.commit()
-    return {"message": "User created"}
 
-@app.route("/contacts/create/", methods=["POST"])
+    return jsonify({"redirect": "/people"})
+
+
+@app.route("/people/")
+def get_all_people():
+    people = Person.query.all()
+    return render_template("people.html", people=people)
+
+
+@app.route("/contacts/add/", methods=["POST", "GET"])
 def create_contact():
-    return {"message": "Contact created"}
+    if request.method == "GET":
+        people = Person.query.all()
+        return render_template("create-contact.html", people=people)
+
+    contact = Contact(**request.form)
+    db.session.add(contact)
+    db.session.commit()
+
+    return jsonify({"redirect": "/contacts"})
 
 
 @app.route("/contacts/")
 def get_all_contacts():
-    return {"message": "Contacts queried"}
-
-@app.route("/users/")
-def get_all_users():
-    users = Person.query.all()
-    print(users)
-    return {"message": "Users queried"}
+    contacts = Contact.query.all()
+    people = Person.query.all()
+    return render_template("contacts.html", contacts=contacts, people=people)
 
 
 # get single contact
@@ -57,13 +74,19 @@ db = SQLAlchemy(app)
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(80), nullable=False)
-    contacts = db.relationship('Contact', backref='person', lazy=True)
+    contacts = db.relationship("Contact", backref="person", lazy=True)
+
+    def __repr__(self):
+        return self.first_name
 
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     contact = db.Column(db.String())
-    person_id = db.Column(db.Integer, db.ForeignKey('person.id'),
-        nullable=False)
+    person_id = db.Column(db.Integer, db.ForeignKey("person.id"), nullable=False)
 
-db.create_all()
+    def __repr__(self):
+        return self.contact
+
+
+# db.create_all()
