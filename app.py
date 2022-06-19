7d8ebf4520cef6820959f9f139e32232456e6a21
wangonya <kwangonya@gmail.com>
2022-06-19 21:15:09 +0300

    html -> json responses

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-import os
 import json
+import os
 
-from flask import Flask, jsonify, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.exceptions import HTTPException
 
 app = Flask(__name__)
@@ -17,56 +16,70 @@ def handle_exception(e):
     # start with the correct headers and status code from the error
     response = e.get_response()
     # replace the body with JSON
-    response.data = json.dumps({
-        "code": e.code,
-        "name": e.name,
-        "description": e.description,
-        "my_custom_message": "dsfh",
-    })
+    response.data = json.dumps(
+        {
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        }
+    )
     response.content_type = "application/json"
     return response
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return {"message": "Hello World!"}
 
 
-@app.route("/people/add", methods=["POST", "GET"])
-def create_person():
-    if request.method == "GET":
-        return render_template("create-person.html")
-
-    person = Person(**request.form)
-    db.session.add(person)
-    db.session.commit()
-
-    return jsonify({"redirect": "/people"})
-
-
-@app.route("/people/")
-def get_all_people():
-    people = Person.query.all()
-    return render_template("people.html", people=people)
-
-
-@app.route("/contacts/add/", methods=["POST", "GET"])
-def create_contact():
+@app.route("/people", methods=["POST", "GET"])
+def people():
     if request.method == "GET":
         people = Person.query.all()
-        return render_template("create-contact.html", people=people)
+        result = []
 
-    contact = Contact(**request.form)
-    db.session.add(contact)
-    db.session.commit()
+        for person in people:
+            result.append(
+                {
+                    "id": person.id,
+                    "first_name": person.first_name,
+                    "contacts": [
+                        {"id": contact.id, "contact": contact.contact}
+                        for contact in person.contacts
+                    ],
+                }
+            )
+        return jsonify(result)
+    else:
+        person = Person(**request.json)
+        db.session.add(person)
+        db.session.commit()
 
-    return jsonify({"redirect": "/contacts"})
+        return {"message": "person created"}, 201
 
 
-@app.route("/contacts/")
-def get_all_contacts():
-    contacts = Contact.query.all()
-    people = Person.query.all()
-    return render_template("contacts.html", contacts=contacts, people=people)
+@app.route("/contacts", methods=["POST", "GET"])
+def contacts():
+    if request.method == "GET":
+        contacts = Contact.query.all()
+        result = []
+
+        for contact in contacts:
+            result.append(
+                {
+                    "id": contact.id,
+                    "contact": contact.contact,
+                    "person_id": contact.person_id,
+                }
+            )
+
+        return jsonify(result)
+    else:
+        contact = Contact(**request.json)
+        db.session.add(contact)
+        db.session.commit()
+
+        return {"message": "contact created"}, 201
 
 
 # get single contact
@@ -88,9 +101,7 @@ DB_NAME = os.getenv("DB_NAME")
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_SERVER}:5432/{DB_NAME}"
-print(app.config[
-    "SQLALCHEMY_DATABASE_URI"
-])
+print(app.config["SQLALCHEMY_DATABASE_URI"])
 db = SQLAlchemy(app)
 
 
@@ -99,17 +110,15 @@ class Person(db.Model):
     first_name = db.Column(db.String(80), nullable=False)
     contacts = db.relationship("Contact", backref="person", lazy=True)
 
-    def __repr__(self):
-        return self.first_name
-
 
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     contact = db.Column(db.String())
-    person_id = db.Column(db.Integer, db.ForeignKey("person.id"), nullable=False)
-
-    def __repr__(self):
-        return self.contact
+    person_id = db.Column(
+        db.Integer,
+        db.ForeignKey("person.id"),
+        nullable=False,
+    )
 
 
 # db.create_all()
